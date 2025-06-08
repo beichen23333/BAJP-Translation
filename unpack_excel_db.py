bb6a9ffe36f257fc1f3b1587a747b510ad91244f
北辰 <3857327758@qq.com>
2025-06-08 16:22:17 +0800

    Update unpack_excel_db.py

--- unpack_excel_db.py
+++ unpack_excel_db.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import sqlite3
+import inspect
 from pathlib import Path
 from setup_utils import dynamic_import_module, convert_to_basic_types, pack_to_zip, deserialize_flatbuffer
 
